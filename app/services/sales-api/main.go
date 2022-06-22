d6fba1d433002111d2cba64b78a23b9821ceacff
Gleb Akishin <ekod87@yandex.ru>
2022-06-22 09:56:50 +0300

    added admin layer

--- app/services/sales-api/main.go
+++ app/services/sales-api/main.go
@@ -17,11 +17,11 @@ import (
 	"github.com/Ekod/go-prod/business/sys/auth"
 	"github.com/Ekod/go-prod/business/sys/database"
 	"github.com/Ekod/go-prod/foundation/keystore"
+	"github.com/Ekod/go-prod/foundation/logger"
 	"github.com/ardanlabs/conf/v3"
 
 	// "go.uber.org/automaxprocs/maxprocs"
 	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
 )
 
 /*
@@ -33,7 +33,7 @@ var build = "develop"
 func main() {
 
 	//Construct the app logger
-	log, err := initLogger("SALES-API")
+	log, err := logger.New("SALES-API")
 	if err != nil {
 		fmt.Println("Error constructing logger:", err)
 		os.Exit(1)
@@ -240,19 +240,3 @@ func run(log *zap.SugaredLogger) error {
 	return nil
 }
 
-func initLogger(service string) (*zap.SugaredLogger, error) {
-	config := zap.NewProductionConfig()
-	config.OutputPaths = []string{"stdout"}
-	config.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
-	config.DisableStacktrace = true
-	config.InitialFields = map[string]interface{}{
-		"service": service,
-	}
-
-	log, err := config.Build()
-	if err != nil {
-		return nil, err
-	}
-
-	return log.Sugar(), nil
-}
